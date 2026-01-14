3c5b16f5d76b231fa2c8c4052c92c8c8691be146
Ivan Revchuk <ivanrevchukpython@gmail.com>
2026-01-14 16:21:18 +0200

    Initial Commit

---
+++ TimerForMac/TimerForMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerForMacApp.swift
+//  TimerForMac
+//
+//  Created by Ivan Revchuk on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TimerForMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
